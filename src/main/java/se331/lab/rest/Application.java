1e0ff9fe63931fda029ec7837544174f692c7819
0ctopusHead <yanwarut_s@cmu.ac.th>
2023-10-06 10:09:32 +0700

    add cors

--- src/main/java/se331/lab/rest/Application.java
+++ src/main/java/se331/lab/rest/Application.java
@@ -19,7 +19,7 @@ public class Application {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://34.230.44.49/:3001")
+                        .allowedOrigins("http://44.201.184.79:8001")
                         .exposedHeaders("x-total-count");
             }
         };
