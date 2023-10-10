81fca0dfb26dc1d7b8d8ced11f0b05b6a0124f9f
0ctopusHead <yanwarut_s@cmu.ac.th>
2023-10-10 09:13:53 +0700

    adsrteyrg

--- src/main/java/se331/lab/rest/security/config/SecurityConfiguration.java
+++ src/main/java/se331/lab/rest/security/config/SecurityConfiguration.java
@@ -39,6 +39,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.GET,"/participants").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                         .requestMatchers(HttpMethod.POST,"/events").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/**").hasRole("ADMIN")
                         .anyRequest().authenticated();
             })
 
